d1df1c56bc514af67c5fc895774b984b854807b3
thanhpham0704 <thanhpham074@gmail.com>
2023-04-04 11:19:00 +0700

    04 april 2023

--- pages/1_🎬_Chi tiết các loại thực thu.py
+++ pages/1_🎬_Chi tiết các loại thực thu.py
@@ -10,7 +10,7 @@ import numpy as np
 import calendar
 
 
-page_title = "Phiếu đăng ký kết thúc"
+page_title = "Chi tiết các loại thực thu"
 page_icon = "🎬"
 layout = "wide"
 st.set_page_config(page_title=page_title, page_icon=page_icon, layout=layout)
@@ -78,6 +78,28 @@ if authentication_status:
             {1: "Hoa Cúc", 2: "Gò Dầu", 3: "Lê Quang Định", 5: "Lê Hồng Phong"})
         return dataframe
 
+    @st.cache_data()
+    def grand_total(dataframe, column):
+        # create a new row with the sum of each numerical column
+        totals = dataframe.select_dtypes(include=[float, int]).sum()
+        totals[column] = "Grand total"
+        # append the new row to the dataframe
+        dataframe = dataframe.append(totals, ignore_index=True)
+        return dataframe
+    # @st.cache_data()
+    # def bar_chart(dataframe, x_value, y_value, text_value, color)
+    # fig1 = px.bar(thucthu_ketthuc_group, x='ketoan_coso',
+    #                 y='thực thu kết thúc', text='thực thu kết thúc', color='ketoan_coso', color_discrete_sequence=['#07a203', '#ffc107', '#e700aa', '#2196f3', ])
+    # fig1.update_layout(
+    #     # Increase font size for all text in the plot)
+    #     xaxis_title='Chi nhánh', yaxis_title='Thực thu kết thúc', showlegend=True, font=dict(size=17))
+    # fig1.update_traces(
+    #     hovertemplate="Thực thu kết thúc: %{y:,.0f}<extra></extra>",
+    #     # Add thousand separators to the text label
+    #     texttemplate='%{text:,.0f}',
+    #     textposition='inside'  # Show the text label inside the bars
+    # )
+
     df = csv_reader("diemdanh_details.csv")
     df1 = collect_filtered_data(table='diemdanh_details', date_column='date_created',
                                 start_time='2023-01-01', end_time='2025-01-01')
@@ -122,11 +144,13 @@ if authentication_status:
         thucthu_ketthuc = rename_lop(thucthu_ketthuc, 'ketoan_coso')
         thucthu_ketthuc_group = thucthu_ketthuc.groupby("ketoan_coso", as_index=False)[
             'thực thu kết thúc'].sum()
+        thucthu_ketthuc_group = grand_total(
+            thucthu_ketthuc_group, "ketoan_coso")
         ""
         st.subheader("Tổng thực thu kết thúc theo chi nhánh")
 
         fig1 = px.bar(thucthu_ketthuc_group, x='ketoan_coso',
-                      y='thực thu kết thúc', text='thực thu kết thúc', color='ketoan_coso', color_discrete_sequence=['#07a203', '#ffc107', '#e700aa', '#2196f3', ])
+                      y='thực thu kết thúc', text='thực thu kết thúc', color='ketoan_coso', color_discrete_map={'Gò Dầu': '#07a203', 'Hoa Cúc': '#ffc107', 'Lê Hồng Phong': '#e700aa', 'Lê Quang Định': '#2196f3', 'Grand total': "White"})
         fig1.update_layout(
             # Increase font size for all text in the plot)
             xaxis_title='Chi nhánh', yaxis_title='Thực thu kết thúc', showlegend=True, font=dict(size=17))
@@ -136,7 +160,6 @@ if authentication_status:
             texttemplate='%{text:,.0f}',
             textposition='inside'  # Show the text label inside the bars
         )
-
         st.plotly_chart(fig1, use_container_width=True)
         thucthu_ketthuc.columns = ['ketoan_id', 'chi nhánh', 'giờ đăng ký', 'tiền trên giờ',
                                    'ngày kết thúc', 'giờ đã học', 'giờ còn lại', 'thực thu kết thúc', 'hv_id']
@@ -150,7 +173,7 @@ if authentication_status:
             # Close the Pandas Excel writer and output the Excel file to the buffer
             writer.save()
             st.download_button(
-                label="Download thực thu kết thúc worksheets",
+                label="Download chi tiết thực thu kết thúc worksheets",
                 data=buffer,
                 file_name="thucthu_ketthuc.xlsx",
                 mime="application/vnd.ms-excel"
@@ -158,3 +181,70 @@ if authentication_status:
     except KeyError:
         st.warning(
             f"Tháng {date(now.year, now.month, 1)} hiện tại chưa có data, bạn chọn tháng trước nhé")
+        # "_______________" thực thu chuyển phí
+    hv_status = collect_data('https://vietop.tech/api/get_data/hv_status')
+    hocvien = collect_data('https://vietop.tech/api/get_data/hocvien')
+    # Filter orders
+    orders_chuyenphi = orders.query("ketoan_active == 5")[["ketoan_id", "hv_id", "ketoan_details", "ketoan_coso", "ketoan_sogio", "ketoan_price",
+                                                           "ketoan_tientrengio", "remaining_time", "kh_id"]]
+    # Filter hv_status
+    hv_status_chuyenphi = hv_status[['ketoan_id', 'status', 'lop_id', 'note', 'is_price', 'created_at']]\
+        .query("status ==7")
+    # Filter hocvien
+    hocvien_chuyenphi = hocvien[['hv_id', 'hv_fullname']]
+    # Merge hv_status and orders
+    chuyenphi = hv_status_chuyenphi.merge(
+        orders_chuyenphi, on='ketoan_id', how='left')
+    # Merge hocvien_chuyephi
+    chuyenphi = chuyenphi.merge(hocvien_chuyenphi, on='hv_id', how='left')
+    chuyenphi = chuyenphi[['created_at', 'hv_fullname',
+                           'ketoan_coso', 'note', 'is_price']]
+    # Add column Phi Chuyen
+    chuyenphi['phí chuyển'] = chuyenphi.is_price * 0.1
+    # Add column Tien con lai sau phi
+    chuyenphi['còn lại sau phí'] = chuyenphi.is_price - \
+        chuyenphi['phí chuyển']
+    # Change data type
+    chuyenphi = chuyenphi.astype(
+        {'created_at': 'datetime64[ns]'})
+    # Sort
+    chuyenphi = chuyenphi.sort_values(by='created_at', ascending=False)
+    chuyenphi = chuyenphi.query(
+        "created_at >= @ketoan_start_time and created_at <= @ketoan_end_time")
+    # Rename columns
+    chuyenphi = rename_lop(chuyenphi, 'ketoan_coso')
+    chuyenphi
+    "---"
+    chuyenphi.columns = ["Ngày tạo", "Họ tên học viên", "Chi nhánh",
+                         "Ghi chú", "Học phí chuyển", "Phí chuyển", "Còn lại sau phí"]
+
+    chuyenphi_group = chuyenphi.groupby('Chi nhánh', as_index=False)[
+        'Phí chuyển'].sum()
+    chuyenphi_group = grand_total(
+        chuyenphi_group, "Chi nhánh")
+    # Create bar_chart
+    fig2 = px.bar(chuyenphi_group, x='Chi nhánh',
+                  y='Phí chuyển', text='Phí chuyển', color='Chi nhánh', color_discrete_map={'Gò Dầu': '#07a203', 'Hoa Cúc': '#ffc107', 'Lê Hồng Phong': '#e700aa', 'Lê Quang Định': '#2196f3', 'Grand total': "White"})
+    fig2.update_layout(
+        # Increase font size for all text in the plot)
+        xaxis_title='Chi nhánh', yaxis_title='Thực thu chuyển phí', showlegend=True, font=dict(size=17))
+    fig2.update_traces(
+        hovertemplate="Thực thu chuyển phí: %{y:,.0f}<extra></extra>",
+        # Add thousand separators to the text label
+        texttemplate='%{text:,.0f}',
+        textposition='inside')  # Show the text label inside the bars
+    st.plotly_chart(fig2, use_container_width=True)
+
+    chuyenphi = chuyenphi.set_index("Ngày tạo")
+    st.subheader("Chi tiết thực thu chuyển phí")
+    st.dataframe(chuyenphi, use_container_width=True)
+    with pd.ExcelWriter(buffer, engine='xlsxwriter') as writer:
+        # Write each dataframe to a different worksheet.
+        chuyenphi.to_excel(writer, sheet_name='Sheet1')
+        # Close the Pandas Excel writer and output the Excel file to the buffer
+        writer.save()
+        st.download_button(
+            label="Download chi tiết thực thu chuyển phí worksheets",
+            data=buffer,
+            file_name="thucthu_chuyenphi.xlsx",
+            mime="application/vnd.ms-excel")
